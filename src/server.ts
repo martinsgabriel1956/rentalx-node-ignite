07ef404a26769e16d42b635b5df5aa94623eb310
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-11 22:11:31 -0300

    feat: setting ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+
+  import express from "express";
+  
+  const app = express();
+
+  const PORT = 3333;
+
+  app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World' });
+  })
+
+  app.listen(PORT, () => {
+    return console.log("Server is running")
+  })
