6ac603cbccaf2021626ee2098db4de5b841e5259
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-13 18:20:43 -0300

    feat: debugging the app

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = 3333;
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' });
 });
+
+app.post('/courses', (request, response) => {
+  console.log(request.body);
+
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
+app.listen(PORT, () => console.log('Server is running!'));
