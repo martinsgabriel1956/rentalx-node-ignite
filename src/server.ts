4020d47e359c051246a0b675f6503ea61c307bcb
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-13 17:39:05 -0300

    fix: adding prettier.config.js and fixing .eslintconfig

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
-import express from "express";
+import express from 'express';
 
-const app = express()
+const app = express();
 
-const PORT = 3333
+const PORT = 3333;
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" })
-});
-
-app.listen(PORT, () => {
-  return console.log("Server is running");
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
