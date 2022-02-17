3e32630e2282b5aee748ecc49e7d766790c549be
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-17 08:58:38 -0300

    feat: refactoring routes

--- src/modules/cars/useCases/createSpecification/index.ts
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -2,7 +2,7 @@ import { SpecificationsRepository } from '../../repositories/implementations/Spe
 import { CreateSpecificationController } from './CreateSpecificationController';
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 
-const specificationsRepository = new SpecificationsRepository();
+const specificationsRepository = SpecificationsRepository.getInstance();
 const specificationUseCase = new CreateSpecificationUseCase(
   specificationsRepository,
 );
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 const PORT = 3333;
 
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(PORT, () => console.log('Server is running!'));
