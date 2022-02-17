a5e6328e558b6743c58ec7d57e3fe2589cf02f11
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-17 08:28:13 -0300

    feat: separating repositories

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,5 +1,5 @@
-import { Category } from '../models/Category';
-import { ICreateCategoryDTO } from './ICategoriesRepository';
+import { Category } from '../../models/Category';
+import { ICreateCategoryDTO } from '../ICategoriesRepository';
 
 class CategoriesRepository {
   private categories: Category[];
